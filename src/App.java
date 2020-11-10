dc99ea6c3e5d20048404985e989bc391bcac49f8
Vadim Rogov <rogov.2099@yandex.com>
2020-11-10 16:44:46 +0300

    added GUI

--- src/App.java
+++ src/App.java
@@ -1,46 +1,74 @@
 import java.util.Random;
-import java.io.DataInputStream;
+import java.awt.Font;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        DataInputStream in = new DataInputStream(System.in);
-        int numWords = 100;
-        int wLength = 3;
+public class App extends JFrame {
+    private static final long serialVersionUID = 1L;
 
-        try {
-            System.out.println("Enter number of words: ");
-            numWords = Integer.parseInt(in.readLine());
+    public static final Font FONT = new Font("Verdana", Font.PLAIN, 11);
+    
+    public static void createGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
 
-            System.out.println("Enter words length: ");
-            wLength = Integer.parseInt(in.readLine());
-        } finally {
-            String[] words = generateRandomWords(numWords, wLength);
-            String[] pWords = new String[0];
-            
-            System.out.println("\n\n *** Palindromic words *** \n\n");
-            for(int i = 0; i < words.length; i++) {
-                if(new StringBuilder(words[i]).reverse().toString().equals(words[i])) {
-                    int length = pWords.length;
-                    pWords = new String[length + 1];
-                    pWords[length] = words[i];
-                    System.out.println(words[i]);
-                }
+        String input1 = JOptionPane.showInputDialog(null, "Enter number of words: ");
+        String input2 = JOptionPane.showInputDialog(null, "Enter words length: ");
+        int numWords = Integer.parseInt(input1);
+        int wLength = Integer.parseInt(input2);
+
+        String[] words = generateRandomWords(numWords, wLength);
+        String[] pWords = new String[0];
+        
+        for(int i = 0; i < words.length; i++) {
+            if(new StringBuilder(words[i]).reverse().toString().equals(words[i])) {
+                int length = pWords.length;
+                pWords = new String[length + 1];
+                pWords[length] = words[i];
             }
         }
+
+        JOptionPane.showMessageDialog(null, stringify(words), "*** Random words ***", JOptionPane.INFORMATION_MESSAGE);
+        
+        String pWordsText = "Nothing to say";
+        if(pWords.length > 0) {
+            pWordsText = stringify(pWords);
+        }
+        JOptionPane.showMessageDialog(null, pWordsText, "*** Palindromic words ***", JOptionPane.INFORMATION_MESSAGE);
+    }
+     
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                UIManager.put("Button.font", FONT);
+                UIManager.put("Label.font", FONT);              
+                JFrame.setDefaultLookAndFeelDecorated(true);
+                JDialog.setDefaultLookAndFeelDecorated(true);
+                createGUI();
+            }
+        });
     }
 
     public static String[] generateRandomWords(int numberOfWords, int wordsLength) {
         String[] randomStrings = new String[numberOfWords];
         Random random = new Random();
-        System.out.println("\n\n *** Random words *** \n\n");
         for(int i = 0; i < numberOfWords; i++) {
             char[] word = new char[wordsLength];
             for(int j = 0; j < word.length; j++) {
                 word[j] = (char)('a' + random.nextInt(26));
             }
             randomStrings[i] = new String(word);
-            System.out.println(randomStrings[i] + '\n');
         }
         return randomStrings;
+    }
+
+    public static String stringify(String[] arr) {
+        String txt = "";
+        for(int i = 0; i < arr.length; i++) {
+            txt = txt + "\n" + arr[i];
+        }
+        return txt;
     }
 }
